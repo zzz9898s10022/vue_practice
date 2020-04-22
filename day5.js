2e7d934387d387bce84b3408d18789ee8cf67f34
zzz9898s10022 <zzz9898s10022@gmail.com>
2020-04-22 12:46:25 +0800

    20200422 雙向資料綁定

---
+++ day5.js
@@ -0,0 +1,12 @@
+
+        let data = {
+          name: "Alex",
+          sex: "男",
+          age: "21~30",
+          hobby: [],
+          member: true,
+        };
+        let vm = new Vue({
+          el: "#app",
+          data: data,
+        });
